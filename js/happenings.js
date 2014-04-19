d8f9aae6f8cee77b8855da3851fd467e5906fc91
Henrik Ekelöf <henrik@hmail.se>
2014-04-19 21:29:42 +0200

    Refactor script, bugfix

--- js/happenings.js
+++ js/happenings.js
@@ -1,198 +1,30 @@
 /*global countdown */
 
-var HAPPENING = {};
+var H = {};
 
-(function (win, doc) {
-    
+(function (win) {
+
     'use strict';
 
-    var happeningContainer, clockContainer, nowContainer, notNowContainer,
-        d, h, m, s, interval, input,
-        happenings, happening, happeningStartDate, happeningEndDate, isCounting;
+    var input = win.location.hash,
+        defaultHappenings, happenings, i;
 
-    
-    happeningContainer = doc.getElementById('happening');
-    clockContainer = doc.getElementById('clock');
-    nowContainer = doc.getElementById('now');
-    notNowContainer = doc.getElementById('not-now');
-
-    d = doc.getElementById('d');
-    h = doc.getElementById('h');
-    m = doc.getElementById('m');
-    s = doc.getElementById('s');
-
-    input = win.location.hash,
-    
-    // 0 = sunday, 6 = saturday
-    happenings = [
+    defaultHappenings = [
         {
             name: 'Fredagsöl',
-            start: {
-                d: 5,
-                h: 16,
-                m: 30,
-                s: 0
-            },
-            end: {
-                d: 5,
-                h: 23,
-                m: 59,
-                s: 59
-            }
+            start: { d: 5, h: 16, m: 30, s: 0 },
+            end: { d: 5, h: 23, m: 59, s: 59 }
         },
         {
             name: 'Onsdagsfika',
-            start: {
-                d: 3,
-                h: 15,
-                m: 0,
-                s: 0
-            },
-            end: {
-                d: 3,
-                h: 16,
-                m: 0,
-                s: 0
-            }
+            start: { d: 3, h: 15, m: 0, s: 0 },
+            end: { d: 3, h: 16, m: 0, s: 0 }
         }
     ];
 
-    function displayHappening() {
-        clockContainer.style.display = 'none';
-        happeningContainer.style.display = 'flex';
-        doc.body.className = 'happening';
-        doc.getElementById('happeninglabel').innerHTML = happening.name;
-    }
+    happenings = defaultHappenings.slice(0);
 
-    function displayCountdown() {
-        clockContainer.style.display = 'flex';
-        happeningContainer.style.display = 'none';
-        doc.body.className = '';
-        doc.getElementById('clocklabel').innerHTML = happening.name;
-    }
-
-    function displayTime(c) {
-        d.innerHTML = c.days;
-        h.innerHTML = c.hours;
-        m.innerHTML = c.minutes;
-        s.innerHTML = c.seconds;
-    }
-
-    function getDate(obj) {
-        var d = new Date(),
-            n = new Date();
-        d.setDate(d.getDate() + ((obj.d + 7) - d.getDay()) % 7);
-        d.setHours(obj.h);
-        d.setMinutes(obj.m);
-        d.setSeconds(obj.s);
-        if (d < n) {
-            d.setDate(d.getDate() + 7);
-        }
-        return d;
-    }
-
-
-    function getNextHappening() {
-        
-        var i, next, previous, current;
-
-        if (happenings.length === 1) {
-            return happenings[0];
-        }
-
-        previous = getDate(happenings[0].start);
-        next = happenings[0];
-
-        for (i = 1; i < happenings.length; i += 1) {
-            current = getDate(happenings[i].start);
-            if (current < previous) {
-                next = happenings[i];
-            }
-        }
-
-        return next;
-
-    }
-
-    function isHappening() {
-        var now = new Date();
-        return (happeningStartDate < now && happeningEndDate > now);
-    }
-    
-
-    function doHappening() {
-        if (isHappening()) {
-            setTimeout(doHappening, 10000);
-        } else {
-            initCountdown();
-        }
-    }
-
-    function countdownToHappening() {
-
-        if (isHappening()) {
-            isCounting = false;
-            displayHappening();
-            doHappening();
-        } else {
-            if (!isCounting) {
-                displayCountdown();
-                isCounting = true;
-            }
-            displayTime(countdown(happeningStartDate));
-            win.requestAnimationFrame(countdownToHappening);
-        }
-
-    }
-
-    function setNextHappening() {
-        happening = getNextHappening();
-        happeningStartDate = getDate(happening.start);
-        happeningEndDate = getDate(happening.end);
-    }
-
-    function initCountdown() {
-        if (win.requestAnimationFrame) {
-            setNextHappening();
-            doc.title = 'When is the next ' + happening.name + '?';
-            countdownToHappening(happeningStartDate);
-        }
-
-    }
-
-    function isItNow() {
-        if (isHappening() && !isCounting) {
-            nowContainer.style.display = 'block';
-            notNowContainer.style.display = 'none';
-            doc.body.className = 'happening';
-            isCounting = true;
-        }
-        if (!isHappening() && isCounting) {
-            nowContainer.style.display = 'none';
-            notNowContainer.style.display = 'block';
-            doc.body.className = '';
-            isCounting = false;
-        }
-    }
-
-    function initIsItNow() {
-        if (input) {
-            happenings = [];
-            parseHappenings(input);
-            happenings = [happenings[0]];
-            setNextHappening();
-            nowContainer.style.display = 'none';
-            notNowContainer.style.display = 'block';
-            doc.getElementById('now-label-1').innerHTML = happening.name;
-            doc.getElementById('now-label-2').innerHTML = happening.name;
-            doc.title = 'Is it ' + happening.name + '?';
-
-            interval = setInterval(isItNow, 10000);
-        }
-    }
-
-
-    function parseHappenings(s) {
+    function getHappeningsFromUrl(s) {
             
         var i, h, tStart, tEnd;
 
@@ -239,13 +71,225 @@ var HAPPENING = {};
         
     }
 
-    if (input) {
-        parseHappenings(input);
+    function Happening(h) {
+
+        function getDate(obj) {
+            var d = new Date();
+            d.setDate(d.getDate() + ((obj.d + 7) - d.getDay()) % 7);
+            d.setHours(obj.h);
+            d.setMinutes(obj.m);
+            d.setSeconds(obj.s);
+            return d;
+        }
+
+        var happening = this;
+
+        this.name = h.name;
+
+        this.startDate = function () {
+            
+            var now = new Date(),
+                start = getDate(h.start),
+                end = getDate(h.end);
+
+            if (end < now) {
+                start.setDate(start.getDate() + 7);
+            }
+
+            return start;
+            
+        };
+
+        this.endDate  = function () {
+
+            var now = new Date(),
+                end = getDate(h.end);
+
+            if (end < now) {
+                end.setDate(end.getDate() + 7);
+            }
+
+            return end;
+
+        };
+        
+        this.isHappening = function () {
+            var now = new Date();
+            return (happening.startDate() <= now && happening.endDate() > now);
+        };
+
     }
 
+    if (input) {
+        getHappeningsFromUrl(input);
+    }
+
+    function getNextHappening() {
+        
+        var i, nextHappening, previousDate, currentDate;
+
+        if (H.happenings.length === 1) {
+            return H.happenings[0];
+        }
+
+        previousDate = H.happenings[0].startDate();
+        nextHappening = H.happenings[0];
+
+        for (i = 1; i < H.happenings.length; i += 1) {
+            
+            if (H.happenings[i].isHappening()) {
+                return H.happenings[i];
+            }
+            
+            currentDate = H.happenings[i].startDate();
+            
+            if (currentDate < previousDate) {
+                nextHappening = H.happenings[i];
+            }
+
+        }
+
+        return nextHappening;
+
+    }
+
+    function setSingleHappening() {
+        if (happenings.length > defaultHappenings.length) {
+            H.happenings = [new Happening(happenings[happenings.length - 1])];
+        } else {
+            H.happenings = [new Happening(happenings[0])];
+        }
+    }
+
+    H.happenings = [];
+
+    for (i = 0; i < happenings.length; i += 1) {
+        H.happenings.push(new Happening(happenings[i]));
+    }
+
+    H.getNextHappening = getNextHappening;
+    H.setSingleHappening = setSingleHappening;
+
+
+}(window));
+
+
+
+(function (win, doc) {
     
-    HAPPENING.initCountdown = initCountdown;
-    HAPPENING.initIsItNow = initIsItNow;
+    'use strict';
+
+    var happeningContainer, clockContainer, nowContainer, notNowContainer,
+        happening, isCounting, happeningStartDate,
+        d, h, m, s;
+    
+    happeningContainer = doc.getElementById('happening');
+    clockContainer = doc.getElementById('clock');
+    nowContainer = doc.getElementById('now');
+    notNowContainer = doc.getElementById('not-now');
+
+    d = doc.getElementById('d');
+    h = doc.getElementById('h');
+    m = doc.getElementById('m');
+    s = doc.getElementById('s');
+
+    function displayCountdown() {
+        clockContainer.style.display = 'flex';
+        happeningContainer.style.display = 'none';
+        doc.body.className = '';
+        doc.getElementById('clocklabel').innerHTML = happening.name;
+    }
+
+    function displayTime(c) {
+        d.innerHTML = c.days;
+        h.innerHTML = c.hours;
+        m.innerHTML = c.minutes;
+        s.innerHTML = c.seconds;
+    }
+
+    function displayHappening() {
+        clockContainer.style.display = 'none';
+        happeningContainer.style.display = 'flex';
+        doc.body.className = 'happening';
+        doc.getElementById('happeninglabel').innerHTML = happening.name;
+    }
+
+    function countownToHappeningEnd() {
+        if (happening.isHappening()) {
+            setTimeout(countownToHappeningEnd, 10000);
+        } else {
+            H.count();
+        }
+    }
+
+    function countdownToHappening() {
+
+        if (happening.isHappening()) {
+            isCounting = false;
+            displayHappening();
+            countownToHappeningEnd();
+        } else {
+            if (!isCounting) {
+                displayCountdown();
+                isCounting = true;
+            }
+            displayTime(countdown(happeningStartDate));
+            win.requestAnimationFrame(countdownToHappening);
+        }
+
+    }
+
+
+
+    H.count = function () {
+        if (win.requestAnimationFrame) {
+            happening = H.getNextHappening();
+            happeningStartDate = happening.startDate();
+            doc.title = 'When is the next ' + happening.name + '?';
+            countdownToHappening();
+        }
+
+    };
+      
+    // Is it now?
+
+    function checkForHappeningEnd() {
+        if (happening.isHappening()) {
+            setTimeout(checkForHappeningEnd, 10000);
+        } else {
+            nowContainer.style.display = 'none';
+            notNowContainer.style.display = 'block';
+            doc.body.className = '';
+            checkForHappening();
+        }
+    }
+
+    function checkForHappening() {
+        if (happening.isHappening()) {
+            nowContainer.style.display = 'block';
+            notNowContainer.style.display = 'none';
+            doc.body.className = 'happening';
+            checkForHappeningEnd();
+        } else {
+            setTimeout(checkForHappening, 1000);
+        }
+    }
+
+
+    H.check = function () {
+
+        H.setSingleHappening();
+        happening = H.getNextHappening();
+    
+        nowContainer.style.display = 'none';
+        notNowContainer.style.display = 'block';
+        doc.getElementById('now-label-1').innerHTML = happening.name;
+        doc.getElementById('now-label-2').innerHTML = happening.name;
+        doc.title = 'Is it ' + happening.name + '?';
+
+        checkForHappening();
+        
+    };
 
     
 }(window, document));
